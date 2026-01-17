5ae0ba2b662b133c13fd8a06a89707b3eb937dfb
slav <vrazumihindima@gmail.com>
2026-01-17 17:51:55 +0300

    views.py CBV created

--- products_proj/views.py
+++ products_proj/views.py
@@ -1,26 +1,50 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.views.generic import ListView, DetailView, TemplateView
 
-def home(request):
-    latest = Product.objects.order_by('-created_at-')[:-5]
+# def home(request):
+#     latest = Product.objects.order_by('-created_at-')[:-5]
+#
+#     print("Latest 5 products:", list(latest.values('id', 'name', 'created_at')))
+#     return render(request, 'home.html', {'latest': latest})
 
-    print("Latest 5 products:", list(latest.values('id', 'name', 'created_at')))
-    return render(request, 'home.html', {'latest': latest})
-
-def index(request):
+class ProductListView(ListView):
     """
     Главная: список продуктов.
-    В шаблоне отображаем изображение, имя и описание (обрезанное до 100 символов).
     """
-    products = Product.objects.select_related('category').order_by('-created_at')
-    return render(request, 'home.html', {'products': products})
+    model = Product
+    template_name = 'home.html'
+    context_object_name = 'products'
+    paginate_by = 9
 
-def product_detail(request, pk):
+    def get_queryset(self):
+        return Product.objects.select_related('category').order_by('-created_at-')
+
+# def index(request):
+#     """
+#     Главная: список продуктов.
+#     В шаблоне отображаем изображение, имя и описание (обрезанное до 100 символов).
+#     """
+#     products = Product.objects.select_related('category').order_by('-created_at')
+#     return render(request, 'home.html', {'products': products})
+
+# def product_detail(request, pk):
+#     """
+#     Страница одного товара: принимает pk, получает объект через ORM и рендерит шаблон.
+#     """
+#     product = get_object_or_404(Product.objects.select_related('category'), pk=pk)
+#     return render(request, 'product_detail.html', {'product': product})
+
+class ProductDetailView(DetailView):
     """
-    Страница одного товара: принимает pk, получает объект через ORM и рендерит шаблон.
+     Страница одного товара
     """
-    product = get_object_or_404(Product.objects.select_related('category'), pk=pk)
-    return render(request, 'product_detail.html', {'product': product})
+    model = Product
+    template_name = 'product_detail.html'
+    context_object_name = 'product'
+
+class ContactsView(TemplateView):
+    template_name = 'contacts.html'
 
 
 
