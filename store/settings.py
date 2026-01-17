61bf5e0f70e007691e58f111c4d83caf8e8cf6c2
slav <vrazumihindima@gmail.com>
2026-01-17 19:02:01 +0300

    additional task

--- store/settings.py
+++ store/settings.py
@@ -131,3 +131,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'webmaster@localhost'
